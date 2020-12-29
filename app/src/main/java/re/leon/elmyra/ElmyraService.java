455b939197e25b361cc42b2eec9a0fad0b4add8a
Yohann Leon <yohann@leon.re>
2020-12-29 23:19:09 +0100

    Added a service to keep track of the torch state

--- app/src/main/java/re/leon/elmyra/ElmyraService.java
+++ app/src/main/java/re/leon/elmyra/ElmyraService.java
@@ -1,6 +1,35 @@
 package re.leon.elmyra;
 
+import android.content.Context;
+import android.hardware.camera2.CameraManager;
 import android.service.voice.VoiceInteractionService;
+import android.widget.Toast;
 
 public class ElmyraService extends VoiceInteractionService {
+    public static boolean isFlashlightOn = false;
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        CameraManager cameraManager = (CameraManager) getBaseContext().getSystemService(Context.CAMERA_SERVICE);
+        cameraManager.registerTorchCallback(torchCallback, null);
+    }
+
+
+    @Override
+    public void onDestroy(){
+        super.onCreate();
+
+        CameraManager cameraManager = (CameraManager) getBaseContext().getSystemService(Context.CAMERA_SERVICE);
+        cameraManager.unregisterTorchCallback(torchCallback);
+    }
+
+    private static CameraManager.TorchCallback torchCallback = new CameraManager.TorchCallback() {
+        @Override
+        public void onTorchModeChanged(String cameraId, boolean enabled) {
+            super.onTorchModeChanged(cameraId, enabled);
+            isFlashlightOn = enabled;
+        }
+    };
 }
--- app/src/main/java/re/leon/elmyra/ElmyraSession.java
+++ app/src/main/java/re/leon/elmyra/ElmyraSession.java
@@ -4,10 +4,12 @@ import android.app.assist.AssistContent;
 import android.app.assist.AssistStructure;
 import android.content.Context;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.service.voice.VoiceInteractionSession;
 import android.util.Log;
+import android.widget.Toast;
 
 public class ElmyraSession extends VoiceInteractionSession {
     public ElmyraSession(Context context) {
@@ -31,17 +33,35 @@ public class ElmyraSession extends VoiceInteractionSession {
         super.onHandleAssist(data, structure, content);
 
         hide();
-        flashLightOn();
-    }
-
-    private void flashLightOn() {
-        CameraManager cameraManager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
 
         try {
-            String cameraId = cameraManager.getCameraIdList()[0];
-            cameraManager.setTorchMode(cameraId, true);
+            CameraManager cameraManager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
+            cameraManager.setTorchMode(getCameraId(getContext()), !ElmyraService.isFlashlightOn);
         } catch (CameraAccessException e) {
             Log.d(getClass().getSimpleName(), e.toString());
         }
+    }
+
+    private String getCameraId(Context context) {
+        CameraManager cameraManager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            String[] cameraList = cameraManager.getCameraIdList();
+
+            for (String cameraId : cameraList) {
+                CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
+
+                boolean isFlashAvailable = characteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                boolean isCameraFacingBack = characteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_BACK;
+
+                if (isFlashAvailable && isCameraFacingBack) {
+                    return cameraId;
+                }
+            }
+        } catch (CameraAccessException e) {
+            Log.d(getClass().getSimpleName(), e.toString());
+        }
+
+        return null;
     }
 }
